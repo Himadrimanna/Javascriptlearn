83f6b27c74236dbcbcb5863c38f22580169cbadb
Himadrimanna <himadrimanna792@gmail.com>
2024-05-09 11:41:50 +0530

    first commit

---
+++ Chp1.js
@@ -0,0 +1,37 @@
+//History
+/*Original name  - (first name) - Mocha
+				   (second name) - livescript
+Developed by the programmer Brandon eich in just 10 Days
+Final name  Mocha - Livescript - Javascript
+In 1997 Microsoft copied javascript features and made its own language
+name Jscript
+Ecma Script - Ecma international is an industry asscociation founded in 1996 dedicated to the standardization
+of information and communication system
+Javascript + rules and documentation = Ecmascript
+Es6 is the biggest update in javascript also known as modern javascript
+*/
+
+//Features
+/*
+1.Casesensitive
+2. Dynamically typed
+3. cross -platform
+4. Interpreted
+5.Object oriented scripting language
+6. Backward compatible
+*/
+
+// Ecma have a technical community known as TC39 had dedicated that after 2015 we release javascript with new features every year (annual release)
+
+// formation of V8engine js and NodeJS
+
+/**
+ * In 2008 google chrome came to existence in browser world which makes use of ecma script but in his own way
+ * Google created a V8 engine for his browser to run Javascript which makes Javscript more Faster than his usual performance hence forth all the browser started making their own engines to run js
+ * The Javascript can only used in Browser untill the engineer called Ryan Dahl who extracted the V8 engine of google chrome and make a server side Javascript called Nodejs
+ * so that Javascript can be run outside the browser for the website
+ */
+
+// Hello world
+// to write or print in Js we use console.log
+console.log('Hello world to Js');
