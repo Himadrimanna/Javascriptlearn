7a104967afcada7012aaf486bc8e4a78bdfc1bb8
Himadrimanna <himadrimanna792@gmail.com>
2024-05-15 23:29:13 +0530

    chp3

---
+++ Chp3.js
@@ -0,0 +1,26 @@
+/**
+ * Just like we follow some rules while speaking english (the grammer) we have some rules to follow while writing a javascript program The set of these rules is called syntax in Javascript.
+ *
+ * What is Variable
+ * A Variable is a container that stores a value
+ * This is very similiar to the container used to store the products
+ *
+ * The Value of a javascript variable can be changed during the execution of a program
+ *
+ * Rules for choosing the variable names
+ * -- Letter ,digits, underscores and $ sign allowed
+ * -- Must begin with a $ _ or a letter
+ * -- Javscript reserved words cannot be used as a variable name
+ * -- Himadri and himadri are different variable  (case sensetive)
+ *
+ *
+ */
+let name = 'Himadri';
+let surname = 'Manna';
+let age = 21;
+let isHappy = true;
+
+console.log(typeof name); // string
+console.log(typeof surname); // string
+console.log(typeof age); //number
+console.log(typeof isHappy); //boolean
