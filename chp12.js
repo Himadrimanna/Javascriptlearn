984172578a61a5c6a2aac14ea5ed308aad7ce6e2
Himadrimanna <himadrimanna792@gmail.com>
2025-01-28 16:01:34 +0530

    Josn

--- chp12.js
+++ chp12.js
@@ -15,7 +15,7 @@ const user = {
 console.log(user.Name);
 console.log(user.name);
 console.log(user['location']);
-console.log(typeof user[mysym]);
+console.log(user[mysym]);
 
 console.log(user);
 user.email = 'himadricustom1@gmail.com';
@@ -36,3 +36,65 @@ user.greetingstwo = function () {
 
 console.log(user.greetings());
 console.log(user.greetingstwo());
+
+//Object nesting
+const regularuser = {
+  email: 'some@gmail.com',
+  fullname: {
+    usefullname: {
+      firstname: 'Himadri',
+      lastname: 'Manna',
+    },
+  },
+};
+
+console.log(regularuser.fullname.usefullname.lastname);
+
+//object merger
+
+const obj1 = { a: 0, b: 1 };
+const obj2 = { c: 'c', d: 'd' };
+
+const obj3 = { obj1, obj2 };
+console.log(obj3);
+const obj4 = Object.assign({}, obj1, obj2); //Object Assign method
+console.log(obj4);
+const obj5 = { ...obj1, ...obj2 }; // Seperator method
+console.log(obj5);
+
+//When Data is comed from Database
+const user2 = [
+  {
+    id: 1,
+    name: 'Himadri Manna',
+  },
+  {
+    id: 2,
+    name: 'Faiz Sayed',
+  },
+];
+
+console.log(user2[1]);
+
+//checking all the key and values present in the object
+const tinderuser = {
+  name: 'Hritik solanki',
+  email: 'hritik@gmail.com',
+  isLoggedin: 'True',
+};
+
+console.log(tinderuser);
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+// Object Destructoring
+const Student = {
+  student1: 'Sharaddha Mishra',
+  student2: 'Krishna Gupta',
+};
+
+const { student1 } = Student; //The destructuring assignment syntax is a JavaScript expression that makes it possible to unpack values from arrays, or properties from objects, into distinct variables.
+console.log(student1);
+const { student2: stud } = Student; //giving name to the destructured assignment and it will be called during execution
+console.log(stud);
